056fef5c5989789c7f8ca5f219584a7fd1d6a14e
gal <anlaganlag>
2020-07-14 17:13:49 +0800

    用useReducer處理counter

--- useReducer/src/App.js
+++ useReducer/src/App.js
@@ -1,22 +1,27 @@
-import React ,{useState} from 'react'
+import React ,{useState,useReducer} from 'react'
+
+function reducer(state,action){
+  return {count:state.count+10}
+}
 
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count:0 })
 
   function increment(){
-    setCount(cur => cur+1)
+    dispatch()
   }
 
+
   function decrement(){
-    setCount(cur => cur-1)
+    // setCount(cur => cur-1)
   }
-  console.log(count**2)
+  // console.log(count**2)
 
   return (
     <div>
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </div>
   )
