47239b5f4fe142585739ad41c62634038dd59bd6
gal <anlaganlag>
2020-07-14 13:56:39 +0800

    存取之前的名字

--- useRef/src/App.js
+++ useRef/src/App.js
@@ -2,20 +2,20 @@ import React ,{useState,useEffect,useRef} from 'react';
 
 export default function App() {
   const [name, setName] = useState('')
-  const inputRef = useRef()
+  const prevName = useRef()
 
-  const focus = ()=>inputRef.current.focus()
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
 
 
   return (
     <div >
         <input
-          ref = {inputRef}
           value={name}
           onChange={e => setName(e.target.value)}/>
-        <div>My name is {name}</div>
+        <div>My name is {name} and it used to be {prevName.current}</div>
 
-        <button onClick={focus} >Focus</button>
     </div>
   )
 }
