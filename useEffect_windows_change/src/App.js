fd9e0144a45cd3194c3e2933ecb31c452a96a51d
gal <anlaganlag>
2020-07-14 15:36:06 +0800

    改變窗口大小

---
+++ useEffect_windows_change/src/App.js
@@ -0,0 +1,18 @@
+import React ,{useState,useEffect,useRef} from 'react';
+
+export default function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+ 
+  const handleResize = () =>{
+    setWindowWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize',handleResize)
+  }, [])
+
+  return (
+    <div>
+        {windowWidth}
+    </div>
+  )
+}
